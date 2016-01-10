367b1ab0a8326cf8fd03ca9d4213c80633f4f45b
Kamil Slowikowski <kslowikowski@gmail.com>
2016-01-10 13:49:07 -0500

    fix issue #7
    Previously, labels were restricted to the x and y ranges defined by their
    respective data points.
    
    Currently, labels will be placed in the entire plotting area, regardless of
    the ranges of their data points.

--- R/geom-text-repel.R
+++ R/geom-text-repel.R
@@ -73,9 +73,6 @@
 #'   to 1.
 #' @param max.iter Maximum number of iterations to try to resolve overlaps.
 #'   Defaults to 2000.
-#' @param expand If \code{TRUE} (the default), allow text labels to be placed
-#'   in the expanded plot area. Otherwise, limit their positions to the range
-#'   of the data.
 #'
 #' @examples
 #'
@@ -119,7 +116,6 @@ geom_text_repel <- function(
   segment.size = 0.5,
   force = 1,
   max.iter = 2000,
-  expand = TRUE,
   na.rm = FALSE,
   show.legend = NA,
   inherit.aes = TRUE
@@ -140,7 +136,6 @@ geom_text_repel <- function(
       segment.size = segment.size,
       force = force,
       max.iter = max.iter,
-      expand = expand,
       ...
     )
   )
@@ -167,16 +162,21 @@ GeomTextRepel <- ggproto("GeomTextRepel", Geom,
     segment.color = "#666666",
     segment.size = 0.5,
     force = 1,
-    max.iter = 2000,
-    expand = TRUE
+    max.iter = 2000
   ) {
     lab <- data$label
     if (parse) {
       lab <- parse(text = lab)
     }
 
+    # Get the x and y limits of the panel area.
+    limits <- data.frame(x = panel_scales$x.range, y = panel_scales$y.range)
+    limits <- coord$transform(limits, panel_scales)
+
+    # Transform the raw data to the panel scales.
     data <- coord$transform(data, panel_scales)
 
+    # The padding around each bounding box.
     pad.x <- convertWidth(box.padding, "npc", valueOnly = TRUE)
     pad.y <- convertHeight(box.padding, "npc", valueOnly = TRUE)
 
@@ -207,7 +207,7 @@ GeomTextRepel <- ggproto("GeomTextRepel", Geom,
     # Fudge factor to make each box slightly wider. This is useful when the
     # user adds a legend to the plot, causing all the labels to squeeze
     # together.
-    fudge.width <- abs(max(data$x) - min(data$x)) / 50
+    fudge.width <- abs(max(limits$x) - min(limits$x)) / 80
     boxes <- lapply(boxes, function(b) {
       # fudge.width <- abs(b['x2'] - b['x1']) / 10
       b['x1'] <- b['x1'] - fudge.width
@@ -215,56 +215,21 @@ GeomTextRepel <- ggproto("GeomTextRepel", Geom,
       b
     })
 
-    # By default, fit the text labels in the expanded plot area.
-    expanded.range <- function(xs, amount = 0.05) {
-      xs <- range(xs)
-      d <- abs(xs[2] - xs[1])
-      c(xs[1] - amount * d - pad.x, xs[2] + amount * d + pad.x)
-    }
-
-    if (expand) {
-      xlims <- expanded.range(data$x)
-      ylims <- expanded.range(data$y)
-    } else {
-      xlims <- range(data$x)
-      ylims <- range(data$y)
-    }
-
-    ws <- repel_boxes(
+    # Repel overlapping bounding boxes away from each other.
+    repel <- repel_boxes(
       do.call(rbind, boxes),
-      xlim = xlims,
-      ylim = ylims,
+      xlim = range(limits$x),
+      ylim = range(limits$y),
       force = force * 1e-6,
       maxiter = max.iter
     )
-
-#     cat("data$x", range(data$x), "\n")
-#     cat("ws$x", range(ws$x), "\n")
-#     cat("data$y", range(data$y), "\n")
-#     cat("ws$y", range(ws$y), "\n")
-
-#     grobs <- textGrob(
-#       lab,
-#       ws$x, ws$y, default.units = "native",
-#       rot = data$angle,
-#       gp = gpar(
-#         col = alpha(data$colour, data$alpha),
-#         fontsize = data$size * .pt,
-#         fontfamily = data$family,
-#         fontface = data$fontface,
-#         lineheight = data$lineheight
-#       ),
-#       check.overlap = FALSE
-#     )
-#
-#     grobs
 
     grobs <- lapply(1:nrow(data), function(i) {
       row <- data[i, , drop = FALSE]
       textRepelGrob(
         lab[i],
-        x = unit(ws$x[i], "native"),
-        y = unit(ws$y[i], "native"),
+        x = unit(repel$x[i], "native"),
+        y = unit(repel$y[i], "native"),
         x.orig = unit(data$x[i], "native"),
         y.orig = unit(data$y[i], "native"),
         box.padding = box.padding,
--- vignettes/ggrepel.md
+++ vignettes/ggrepel.md
@@ -1,10 +1,10 @@
 ---
-title: "ggrepel"
+title: "ggrepel Usage Examples"
 author: "Kamil Slowikowski"
-date: "2016-01-07"
+date: "2016-01-10"
 output: rmarkdown::html_vignette
 vignette: >
-  %\VignetteIndexEntry{Vignette Title}
+  %\VignetteIndexEntry{ggrepel Usage Examples}
   %\VignetteEngine{knitr::rmarkdown}
   %\VignetteEncoding{UTF-8}
 ---
@@ -53,13 +53,14 @@ using `geom_text_repel` instead:
 
 ```r
 library(ggrepel)
+set.seed(42)
 ggplot(mtcars) +
   geom_point(aes(wt, mpg), color = 'red') +
   geom_text_repel(aes(wt, mpg, label = rownames(mtcars))) +
   theme_classic(base_size = 16)
 ```
 
-<img src="https://github.com/slowkow/ggrepel/blob/master/vignettes/figures/ggrepel/geom_text_repel-1.png" title="" alt="" width="700" />
+<img src="https://github.com/slowkow/ggrepel/blob/master/vignettes/figures/ggrepel/geom_text_repel-1.png" title="plot of chunk geom_text_repel" alt="plot of chunk geom_text_repel" width="700" />
 
 #### Options
 
@@ -81,11 +82,10 @@ However, the following parameters are not supported:
 - `box.padding` is the padding surrounding the text bounding box
 - `force` is the force of repulsion between overlapping text labels
 - `max.iter` is the maximum number of iterations to attempt to resolve overlaps
-- `expand` the text will be arranged in the expanded plot area if TRUE, or else
-  the text will be arranged within the range of the data points
 
 
 ```r
+set.seed(42)
 ggplot(mtcars) +
   geom_point(aes(wt, mpg), color = 'red') +
   geom_text_repel(
@@ -99,8 +99,7 @@ ggplot(mtcars) +
     segment.color = 'red',
     box.padding = unit(0.3, 'lines'),
     force = 2,
-    max.iter = 1e4,
-    expand = TRUE
+    max.iter = 1e4
   ) +
   scale_color_discrete(name = 'cyl') +
   theme_classic(base_size = 16)
@@ -114,21 +113,46 @@ ggplot(mtcars) +
 
 
 ```r
-library(ggrepel)
-set.seed(100)
+set.seed(42)
 ggplot(mtcars) +
   geom_point(aes(wt, mpg)) +
   geom_label_repel(
     aes(wt, mpg, fill = factor(cyl), label = rownames(mtcars)),
     fontface = 'bold', color = 'white',
-    # box.padding = unit(0.5, "lines"),
-    # label.padding = unit(0.5, "lines")
     box.padding = unit(0.25, "lines")
   ) +
   theme_classic(base_size = 16)
 ```
 
 <img src="https://github.com/slowkow/ggrepel/blob/master/vignettes/figures/ggrepel/geom_label_repel-1.png" title="plot of chunk geom_label_repel" alt="plot of chunk geom_label_repel" width="700" />
+
+## R Session Info
+
+
+```r
+sessionInfo()
+```
+
+```
+## R version 3.2.3 (2015-12-10)
+## Platform: x86_64-apple-darwin13.4.0 (64-bit)
+## Running under: OS X 10.10.5 (Yosemite)
+## 
+## locale:
+## [1] en_US.UTF-8/en_US.UTF-8/en_US.UTF-8/C/en_US.UTF-8/en_US.UTF-8
+## 
+## attached base packages:
+## [1] stats     graphics  grDevices utils     datasets  methods   base     
+## 
+## other attached packages:
+## [1] ggrepel_0.3   ggplot2_2.0.0 knitr_1.12   
+## 
+## loaded via a namespace (and not attached):
+##  [1] Rcpp_0.12.2      codetools_0.2-14 digest_0.6.9     grid_3.2.3      
+##  [5] plyr_1.8.3       gtable_0.1.2     formatR_1.2.1    magrittr_1.5    
+##  [9] evaluate_0.8     scales_0.3.0     stringi_1.0-1    labeling_0.3    
+## [13] tools_3.2.3      stringr_1.0.0    munsell_0.4.2    colorspace_1.2-6
+```
 
 [geom_text]: http://docs.ggplot2.org/current/geom_text.html
 [geom_label]: http://docs.ggplot2.org/current/geom_text.html
