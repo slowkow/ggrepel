55d29d630a3343286ee927cef6c3f1b51b5ef9f2
Sean Hughes <smhughes@uw.edu>
2017-07-08 09:31:38 -0700

    Fix errors in tests.

--- tests/testthat/test-to_unit.R
+++ tests/testthat/test-to_unit.R
@@ -40,17 +40,17 @@ test_that("non-default values as units work with geom_label_repel", {
       point.padding = unit(3, "lines"), label.r = unit(4, "lines"),
       min.segment.length = unit(5, "lines"))
 
-  expect_identical(extract_param(p, "box.padding"), unit(0.25, "lines"))
-  expect_identical(extract_param(p, "label.padding"), unit(0.25, "lines"))
-  expect_identical(extract_param(p, "point.padding"), unit(1e-06, "lines"))
-  expect_identical(extract_param(p, "label.r"), unit(0.15, "lines"))
-  expect_identical(extract_param(p, "min.segment.length"), unit(0.5, "lines"))
+  expect_identical(extract_param(p, "box.padding"), unit(1, "lines"))
+  expect_identical(extract_param(p, "label.padding"), unit(2, "lines"))
+  expect_identical(extract_param(p, "point.padding"), unit(3, "lines"))
+  expect_identical(extract_param(p, "label.r"), unit(4, "lines"))
+  expect_identical(extract_param(p, "min.segment.length"), unit(5, "lines"))
 })
 
 test_that("non-default values as units work with geom_text_repel", {
   d <- data.frame(x = rnorm(10), y = rnorm(10), b = letters[1:10])
   p <- ggplot(d, aes(x, y, text = b)) + geom_point() +
-    geom_label_repel(box.padding = unit(1, "lines"),
+    geom_text_repel(box.padding = unit(1, "lines"),
       point.padding = unit(2, "lines"),
       min.segment.length = unit(3, "lines"))
 
@@ -75,7 +75,7 @@ test_that("default values as numbers work with geom_label_repel", {
 test_that("default values as numbers work with geom_text_repel", {
   d <- data.frame(x = rnorm(10), y = rnorm(10), b = letters[1:10])
   p <- ggplot(d, aes(x, y, text = b)) + geom_point() + +
-    geom_label_repel(box.padding = 0.25, point.padding = 1e-06,
+    geom_text_repel(box.padding = 0.25, point.padding = 1e-06,
       min.segment.length = 0.5)
 
   expect_identical(extract_param(p, "box.padding"), unit(0.25, "lines"))
@@ -99,7 +99,7 @@ test_that("non-default values as numbers work with geom_label_repel", {
 test_that("non-default values as numbers work with geom_text_repel", {
   d <- data.frame(x = rnorm(10), y = rnorm(10), b = letters[1:10])
   p <- ggplot(d, aes(x, y, text = b)) + geom_point() +
-    geom_label_repel(box.padding = 1, point.padding = 2,
+    geom_text_repel(box.padding = 1, point.padding = 2,
       min.segment.length = 3)
 
   expect_identical(extract_param(p, "box.padding"), unit(1, "lines"))
@@ -123,7 +123,7 @@ test_that("non-default values as mix of units and numbers work with geom_label_r
 test_that("non-default values as mix of units and numbers work with geom_text_repel", {
   d <- data.frame(x = rnorm(10), y = rnorm(10), b = letters[1:10])
   p <- ggplot(d, aes(x, y, text = b)) + geom_point() +
-    geom_label_repel(box.padding = 1, point.padding = unit(2, "lines"),
+    geom_text_repel(box.padding = 1, point.padding = unit(2, "lines"),
       min.segment.length = 3)
 
   expect_identical(extract_param(p, "box.padding"), unit(1, "lines"))
@@ -147,7 +147,7 @@ test_that("non-line units work with geom_label_repel", {
 test_that("non-line units work with geom_text_repel", {
   d <- data.frame(x = rnorm(10), y = rnorm(10), b = letters[1:10])
   p <- ggplot(d, aes(x, y, text = b)) + geom_point() +
-    geom_label_repel(box.padding = 1, point.padding = unit(2, "cm"),
+    geom_text_repel(box.padding = 1, point.padding = unit(2, "cm"),
       min.segment.length = 3)
 
   expect_identical(extract_param(p, "box.padding"), unit(1, "lines"))
