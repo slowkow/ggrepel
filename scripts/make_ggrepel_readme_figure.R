4cea479634b3b951e35877153e165210047014e6
Kamil Slowikowski <kslowikowski@gmail.com>
2018-02-11 13:49:45 -0500

    optimize fig.png

--- scripts/make_ggrepel_readme_figure.R
+++ scripts/make_ggrepel_readme_figure.R
@@ -1,11 +1,26 @@
+optimize_png <- function(filename) {
+  optimized <- sub(".png", "-fs8.png", filename)
+  command <- sprintf(
+    "pngquant --speed=1 --quality=0-2 -f %s && mv -f %s %s",
+    filename, optimized, filename
+  )
+  system(command)
+}
+
 library(ggrepel)
-ggplot(mtcars, aes(wt, mpg, label = rownames(mtcars))) +
+set.seed(42)
+p <- ggplot(mtcars, aes(wt, mpg, label = rownames(mtcars))) +
   geom_text_repel(
     size = 1.75,
     segment.size = 0.2,
     box.padding = 0.13, max.iter = 1e4
   ) +
   geom_point(color = 'red', size = 0.5) +
-  theme_classic(base_size = 7)
-ggsave("fig.png", width = 4, height = 2.5)
-system("pngquant -f fig.png && mv -f fig-fs8.png fig.png")
+  theme_classic(base_size = 7) +
+  theme(
+    axis.line = element_line(size = 0.2),
+    axis.ticks = element_line(size = 0.2)
+  )
+ggsave(filename = "fig.png", plot = p, width = 4, height = 2.5)
+optimize_png("fig.png")
+file.info("fig.png")$size
