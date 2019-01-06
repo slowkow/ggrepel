eba556662ebb18e7a3f153ab987300b6f7cfcca7
kiendang <kiend@pm.me>
2019-01-06 12:59:20 +0800

    Fix #96

--- R/geom-label-repel.R
+++ R/geom-label-repel.R
@@ -379,8 +379,6 @@ makeLabelRepelGrobs <- function(
     convertHeight(y.orig, "native", TRUE)
   )
 
-  center <- centroid(c(x1, y1, x2, y2), hjust, vjust)
-
   # Get the coordinates of the intersection between the line from the
   # original data point to the centroid and the rectangle's edges.
   text_box <- c(x1, y1, x2, y2)
@@ -403,7 +401,7 @@ makeLabelRepelGrobs <- function(
       point_pos[1] - point_padding_x, point_pos[2] - point_padding_y,
       point_pos[1] + point_padding_x, point_pos[2] + point_padding_y
     )
-    point_pos <- intersect_line_rectangle(center, point_pos, point_box)
+    point_pos <- intersect_line_rectangle(int, point_pos, point_box)
   }
 
   # Compute the distance between the data point and the edge of the text box.
