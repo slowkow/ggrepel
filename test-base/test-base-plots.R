3244eca89f08518d44b2a7269f93d4301434b2cd
Michael Chirico <michael.chirico@grabtaxi.com>
2020-05-08 20:04:06 +0800

    tracking images

--- test-base/test-base-plots.R
+++ test-base/test-base-plots.R
@@ -1,23 +1,32 @@
+library(ggrepel)
+source('R/RcppExports.R')
+source('R/base-repel.R')
+
+old_wd = setwd('test-base')
+
+fig_dir = 'figures'
+dir.create(fig_dir, showWarnings = FALSE)
+open_pdf = function(filename, dim) {
+    pdf(file.path(fig_dir, paste0(filename, '.pdf')),
+        width = 7*dim[2L], height = 7*dim[1L])
+    par(mfrow = dim, mar = c(0, 0, 0, 0), ps = 14)
+}
+
 x = mtcars$wt
 y = mtcars$mpg
 labels = rownames(mtcars)
 
-repel_boxes2 = ggrepel:::repel_boxes2
-open_pdf = function(filename, dim)
-    pdf(filename, width = 7*dim[2L], height = 7*dim[1L])
-
 # ---- DEFAULTS ----
-dim = 1:2
-open_pdf('
-par(mfrow = 1:2)
+open_pdf('defaults', 1:2)
 plot(x, y, main = 'text()')
 text(x, y, labels)
 
 plot(x, y, main = 'repel_text()')
 text_repel(x, y, labels)
+dev.off()
 
 # ---- ADJ ----
-par(mfrow = c(3L, 3L), mar = c(0, 0, 0, 0))
+open_pdf('adj', c(3L, 3L))
 for (xadj in c(0, .5, 1)) {
     for (yadj in c(0, .5, 1)) {
         plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
@@ -27,9 +36,10 @@ for (xadj in c(0, .5, 1)) {
         text_repel(x, y, labels, adj = c(xadj, yadj))
     }
 }
+dev.off()
 
 # ---- POS ----
-par(mfrow = c(2L, 2L))
+open_pdf('pos', c(2L, 2L))
 for (pos in 1:4) {
     plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
     box()
@@ -37,9 +47,10 @@ for (pos in 1:4) {
     text(x, y, labels, pos = pos, col = 'gray')
     text_repel(x, y, labels, pos = pos)
 }
+dev.off()
 
 # ---- POS WITH OFFSET ----
-par(mfrow = 3:4)
+open_pdf('offset', 3:4)
 for (offset in c(0, .5, 1)) {
     for (pos in 1:4) {
         plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
@@ -49,9 +60,10 @@ for (offset in c(0, .5, 1)) {
         text_repel(x, y, labels, pos = pos, offset = offset)
     }
 }
+dev.off()
 
 # --- VFONT ---
-par(mfrow = c(3L, 3L), mar = c(0, 0, 0, 0))
+open_pdf('vfont', c(3L, 3L))
 for (ii in sample(nrow(Hershey$allowed), 9L)) {
     vfont = c(
         Hershey$typeface[Hershey$allowed[ii, 1L]],
@@ -63,9 +75,10 @@ for (ii in sample(nrow(Hershey$allowed), 9L)) {
     text(x, y, labels, vfont = vfont, col = 'gray')
     text_repel(x, y, labels, vfont = vfont)
 }
+dev.off()
 
 # ---- CEX ----
-par(mfrow = c(3L, 3L), mar = c(0, 0, 0, 0))
+open_pdf('cex', c(3L, 3L))
 for (cex in c(.5, .6, .8, .9, 1, 1.1, 1.25, 1.33, 2, 5)) {
     plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
     box()
@@ -73,19 +86,21 @@ for (cex in c(.5, .6, .8, .9, 1, 1.1, 1.25, 1.33, 2, 5)) {
     text(x, y, labels, cex = cex, col = 'gray')
     text_repel(x, y, labels, cex = cex)
 }
+dev.off()
 
 # ---- CEX AS VECTOR ----
-par(mfrow = c(1L, 1L))
+open_pdf('cex_vector', c(1L, 1L))
 plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
 box()
 cex = runif(length(x), .5, 1.5)
 text(x, y, labels, cex = cex, col = 'gray')
 text_repel(x, y, labels, cex = cex)
+dev.off()
 
 # ---- FONT ----
 ## per src/library/graphics/src/plot.c:FixupFont, font
 ##   can be 1,2,3,4,5 on Unix, up to 32 on Windows
-par(mfrow = c(2L, 2L), mar = c(0, 0, 0, 0))
+open_pdf('font', c(2L, 2L))
 for (font in 1:4) {
     plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
     box()
@@ -93,17 +108,19 @@ for (font in 1:4) {
     text(x, y, labels, font = font, col = 'gray')
     text_repel(x, y, labels, font = font)
 }
+dev.off()
 
 # ---- FONT AS VECTOR ----
-par(mfrow = c(1L, 1L))
+open_pdf('font_vector', c(1L, 1L))
 plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
 box()
 font = sample(1:4, length(x), replace = TRUE)
 text(x, y, labels, font = font, col = 'gray')
 text_repel(x, y, labels, font = font)
+dev.off()
 
 # ---- PAR(PS) ----
-par(mfrow = c(2L, 2L), mar = c(0, 0, 0, 0))
+open_pdf('ps', c(2L, 2L))
 old = par('ps')
 for (ps in c(8, 12, 18, 24)) {
     plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
@@ -114,9 +131,10 @@ for (ps in c(8, 12, 18, 24)) {
     text_repel(x, y, labels)
 }
 par(ps = old)
+dev.off()
 
 # ---- POINT.PADDING ----
-par(mfrow = c(2L, 2L), mar = c(0, 0, 0, 0))
+open_pdf('point_padding', c(2L, 2L))
 for (padding in c(0, .5, 1, 2)) {
     plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
     box()
@@ -124,9 +142,10 @@ for (padding in c(0, .5, 1, 2)) {
     text(x, y, labels, col = 'gray')
     text_repel(x, y, labels, point.padding = padding)
 }
+dev.off()
 
 # ---- FORCE, FORCE_PULL----
-par(mfrow = c(3L, 3L), mar = c(0, 0, 0, 0))
+open_pdf('force', c(3L, 3L))
 for (force in c(.5, 1, 5)) {
     for (force_pull in c(.5, 1, 5)) {
         plot(x, y, axes = FALSE, main = '', xlab = '', ylab = '')
@@ -136,6 +155,7 @@ for (force in c(.5, 1, 5)) {
         text_repel(x, y, labels, force = force, force_pull = force_pull)
     }
 }
+dev.off()
 
 # ---- MAX.TIME ----
 
@@ -146,3 +166,5 @@ for (force in c(.5, 1, 5)) {
 # ---- DIRECTION ----
 
 # ---- ERRORS ----
+
+setwd(old_wd)
